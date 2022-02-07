000ac528a73e64df5ad16239183c87db56128b53
jnyshin <61779849+jnyshin@users.noreply.github.com>
2022-02-07 16:44:38 -0500

    done hw0

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.redirect("/hw0.html");
+});
+
+app.get("/hw0.html", function (req, res) {
+  res.sendFile(path.join(__dirname, "/hw0.html"));
+});
+
+app.get("/image/andrew-schultz-DTSDD968Mpw-unsplash.jpg", function (req, res) {
+  res.sendFile(
+    path.join(__dirname, "/image/andrew-schultz-DTSDD968Mpw-unsplash.jpg")
+  );
+});
+app.listen(80 || 443);
