1fd621497926e02619e272b82ca90eb1d4fe45c3
Yejin Shin <61779849+jnyshin@users.noreply.github.com>
2022-02-08 18:23:48 +0900

    Update server.js

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.get("/image/andrew-schultz-DTSDD968Mpw-unsplash.jpg", function (req, res) {
   );
 });
 
-app.get("/hw1.yml", sendYML, function (req, res) {
+app.get("/hw1.yml", function (req, res) {
   console.log("redirect to /hw0.html");
-  //res.sendFile(path.join(__dirname, "/../hw1.yml"));
-  res.redirect("/hw0.html");
+  res.sendFile(path.join(__dirname, "/../hw1.yml"));
+  //res.redirect("/hw0.html");
 });
 
 function sendYML(req, res, next) {
